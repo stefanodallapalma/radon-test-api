85e7e17a0be838e83f6196c56ae4740c370c32bf
Stefano Dalla Palma <stefano.dallapalma0@gmail.com>
2020-11-03 17:44:52 +0100

    second commit

--- app.py
+++ app.py
@@ -1,26 +1,51 @@
-from flask import render_template
-import connexion
-from flask_cors import CORS
+# app.py
+from flask import Flask, request, jsonify
+app = Flask(__name__)
 
-# Create the application instance
-app = connexion.App(__name__, specification_dir='./')
-CORS(app.app)
+@app.route('/getmsg/', methods=['GET'])
+def respond():
+    # Retrieve the name from url parameter
+    name = request.args.get("name", None)
 
-# Read the swagger.yml file to configure the endpoints
-app.add_api('classification.yml')
-app.add_api('metrics.yml')
-app.add_api('models.yml')
+    # For debugging
+    print(f"got name {name}")
 
-# Create a URL route in our application for "/"
+    response = {}
+
+    # Check if user sent a name at all
+    if not name:
+        response["ERROR"] = "no name found, please send a name."
+    # Check if the user entered a number not a name
+    elif str(name).isdigit():
+        response["ERROR"] = "name can't be numeric."
+    # Now the user entered a valid name
+    else:
+        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+
+    # Return the response in json format
+    return jsonify(response)
+
+@app.route('/post/', methods=['POST'])
+def post_something():
+    param = request.form.get('name')
+    print(param)
+    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
+    if param:
+        return jsonify({
+            "Message": f"Welcome {name} to our awesome platform!!",
+            # Add this option to distinct the POST request
+            "METHOD" : "POST"
+        })
+    else:
+        return jsonify({
+            "ERROR": "no name found, please send a name."
+        })
+
+# A welcome message to test our server
 @app.route('/')
-def home():
-    """
-    This function just responds to the browser ULR
-    localhost:5000/
-    :return: 'home'
-    """
-    return 'home.html goes here'
+def index():
+    return "<h1>Welcome to our server !!</h1>"
 
-# If we're running in stand alone mode, run the application
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
