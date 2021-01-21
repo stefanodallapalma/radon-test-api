b43149abff7b006ec653fcfe13eb812119698652
Stefano Dalla Palma <stefano.dallapalma0@gmail.com>
2021-01-21 10:54:08 +0100

    refactoring

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
 # app.py
 import os
 from flask import Flask, request, jsonify, send_file
+
 app = Flask(__name__)
 
 
 @app.route('/models/', methods=['GET'])
-def respond():
+def models():
     # Retrieve the name from url parameter
     language = request.args.get("language", None)
     response = {}
 
     # Check if user sent a name at all
     if not language:
-        response["ERROR"] = "no language found, please send a language."
+        response["ERROR"] = "No language found, please send a language."
         return response
     elif language == 'ansible':
-        model = os.path.join(os.path.join('models', 'radondp_model_ansible.joblib'))
+        model = os.path.join(os.path.join('models', 'ansible', 'radondp_model_ansible.joblib'))
     elif language == 'tosca':
-        model = os.path.join(os.path.join('models', 'radondp_model_tosca.joblib'))
+        model = os.path.join(os.path.join('models', 'tosca', 'radondp_model_tosca.joblib'))
     else:
         response["ERROR"] = "language not supported."
         return response
@@ -29,6 +30,7 @@ def respond():
 @app.route('/')
 def index():
     return "<h1>Welcome to our server !!</h1>"
+
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
