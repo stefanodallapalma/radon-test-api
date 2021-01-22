4bed132ceb52ea4b69124dde68bd5fab433013e5
Stefano Dalla Palma <stefano.dallapalma0@gmail.com>
2021-01-22 15:09:53 +0100

    Added models and predictions APIs

---
+++ README.md
@@ -0,0 +1,116 @@
+# Radon-defect-prediction-endpoints
+
+## Quick start
+
+### Get model
+Get a pre-trained model from the most similar project.
+
+`GET https://<ip>/models?parm1=value1&...&paramN=valueN`
+
+**Example:**
+`GET http://<ip>/models?language=ansible&repository_size=560&comments_ratio=0.03&has_license=1`
+
+**Parameters**
+
+| Name | Value | Default|
+|------|-------|-----------|
+|`language`| string | None. Choose between {ansible, tosca} |
+|`return_model` | int [0: false, 1: true] | 0 | 
+|`comments_ratio` | numeric | 0 |
+|`commit_frequency`| numeric | 0 |
+|`core_contributors`| numeric | 0 |
+|`has_ci`| [0: false, 1: true] | 0 |
+|`has_license`| [0: false, 1: true] | 0 |
+|`iac_ratio`| numeric | 0 |
+|`issue_frequency`| numeric | 0 |
+|`repository_size`| numeric | 0 |
+
+**Return**
+
+A json containing the model id to use for calls to the `predictions/` endpoint, and a similarity score ([0-1]) between 
+the client's and model's projects. 
+
+```
+{
+    "model_id": 24242603,
+    "similarity": 0.9999
+}
+```
+
+If `return_model=true`, it returns the raw `joblib` model.
+Once saved somewhere, the model can be loaded and used in Python as follows:
+
+```python
+import joblib
+
+model = joblib.load('path/to/model.joblib', mmap_mode='r')
+    
+selected_features = model['selected_features']
+normalizer = model['estimator'].named_steps['normalization']
+tree_classifier = model['estimator'].named_steps['classification']
+
+# selected_features can be used to reduce a new data frame to the same subset of model features
+# normalizer (if not None) must be used to normalize the new data in the same fashion as model's training data
+# tree_classifier can be used for predicting failure-prone script
+``` 
+
+
+
+
+### Predict
+Predict the failure-proneness of a file represented by a set of metrics.
+
+`GET https://<ip>/predictions?parm1=value1&...&paramN=valueN`
+
+**Example:** 
+`GET http://127.0.0.1:5000/predictions?language=ansible&model_id=24242603&num_names_with_vars=10&num_ignore_errors=3&num_conditions=1`
+
+**Parameters**
+
+| Name | Value | Default|
+|------|-------|-----------|
+|`language`| string | None. Choose between {ansible, tosca} |
+|`model_id` | int | None. It is mandatory. Use the model_id from the `/models` endpoint | 
+
+The remaining parameters are the Ansible or Tosca metrics that can be extracted by the script.
+**See how** extract those metrics with [AnsibleMetrics](https://github.com/radon-h2020/radon-ansible-metrics) and 
+[ToscaMetrics](https://github.com/radon-h2020/radon-tosca-metrics).
+
+**Note:** Metrics are passed instead of the raw script to avoid information disclosure.
+
+**Result**
+A json file containing the prediction (i.e., `failure_prone: true/false`), and the decision that led to the prediction 
+(absent if `failure_prone: false`).
+
+```
+{
+  "failure_prone": true,
+  "decision": [
+    [
+      "num_names_with_vars",
+      ">",
+      2.4
+    ],
+    [
+      "num_ignore_errors",
+      "<=",
+      4.16
+    ],
+    [
+      "num_conditions",
+      "<=",
+      17.12
+    ],
+    [
+      "num_filters",
+      "<=",
+      0.0
+    ]
+  ]
+}
+```
+
+In this example, a script has been predicted as **failure-prone** because:
+
+num_names_with_vars > 2.4 **AND** num_ignore_errors <= 4.16 **AND** num_conditions <= 17.12 **AND** num_filters <= 0.
+
--- app.py
+++ app.py
@@ -1,29 +1,173 @@
 # app.py
+import json
+import joblib
 import os
-from flask import Flask, request, jsonify, send_file
+import numpy as np
+import pandas as pd
+
+from flask import Flask, request, send_file
+from scipy.spatial import distance
 
 app = Flask(__name__)
+
+with open(os.path.join('models', 'ansible', 'metadata.json')) as f:
+    ansible_models_metadata = json.load(f)
+
+with open(os.path.join('models', 'tosca', 'metadata.json')) as f:
+    tosca_models_metadata = json.load(f)
 
 
 @app.route('/models/', methods=['GET'])
 def models():
+    """ Get a defect prediction model from the most similar project.
+    The client's project is compared with all the projects for which a defect prediction model exists for the language
+    requested.
+    Projects similarity is computed as the cosine similarity between the client's project metrics vector and any of the
+    available projects'.
+
+    Return
+    ------
+    response: json/file
+        A json response with the project id to use in future calls of /prediction, if return_model = False.
+        Otherwise, the model.
+    """
+
     # Retrieve the name from url parameter
     language = request.args.get("language", None)
+    return_model = request.args.get("return_model", False)
+
+    client_project_metrics = [
+        float(request.args.get("comments_ratio", 0)),
+        float(request.args.get("commit_frequency", 0)),
+        float(request.args.get("core_contributors", 0)),
+        int(request.args.get("has_ci", False)),
+        int(request.args.get("has_license", False)),
+        float(request.args.get("iac_ratio", 0)),
+        float(request.args.get("issue_frequency", 0)),
+        float(request.args.get("repository_size", 0))
+    ]
+
+    response = {}
+    metadata = []
+
+    if language == 'ansible':
+        metadata = ansible_models_metadata
+    elif language == 'tosca':
+        metadata = tosca_models_metadata
+    else:
+        response["ERROR"] = "Set a valid language."
+        return response
+
+    project_id = None
+    path_to_model = None
+    most_similar_score = 0
+
+    for project in metadata:
+        project_metrics = [
+            project["comments_ratio"],
+            project["commit_frequency"],
+            project["core_contributors"],
+            int(project["has_ci"]),
+            int(project["has_license"]),
+            project["iac_ratio"],
+            project["issue_frequency"],
+            project["repository_size"]
+        ]
+
+        sim = 1 - distance.cosine(project_metrics, client_project_metrics)
+        if sim > most_similar_score:
+            most_similar_score = sim
+            project_id = project['id']
+            path_to_model = project['model']
+
+        response['model_id'] = project_id
+        response['similarity'] = sim
+
+    return send_file(path_to_model, as_attachment=True) if return_model else response
+
+
+@app.route('/predictions/', methods=['GET'])
+def predict():
     response = {}
 
-    # Check if user sent a name at all
-    if not language:
-        response["ERROR"] = "No language found, please send a language."
-        return response
-    elif language == 'ansible':
-        model = os.path.join(os.path.join('models', 'ansible', 'ansible__workshops.joblib'))
-    elif language == 'tosca':
-        model = os.path.join(os.path.join('models', 'tosca', 'radondp_model_tosca.joblib'))
-    else:
-        response["ERROR"] = "language not supported."
+    language = None
+    model_id = 0
+    metrics = {}
+
+    for k, v in dict(request.args).items():
+        if k == 'language':
+            language = str(v).lower()
+        elif k == 'model_id':
+            model_id = int(v)
+        else:
+            metrics[k] = float(v)
+
+    unseen_data = pd.DataFrame(metrics, index=[0])
+
+    if language == 'ansible':
+        i = 0
+        while i < len(ansible_models_metadata) and ansible_models_metadata[i]['id'] != model_id:
+            i += 1
+
+        if i < len(ansible_models_metadata):
+            path_to_model = ansible_models_metadata[i]['model']
+            print(path_to_model)
+        else:
+            response["ERROR"] = "Model not found."
+            return response
+
+        model = joblib.load(path_to_model, mmap_mode='r')
+        tree_clf = model['estimator'].named_steps['classification']
+
+        for feature in model['selected_features']:
+            if feature not in unseen_data.columns:
+                unseen_data[feature] = 0
+
+        # Reduce unseen_data to the same subset of features
+        unseen_data = unseen_data[np.intersect1d(unseen_data.columns, model['selected_features'])]
+
+        features_name = unseen_data.columns
+
+        # Perform pre-process if any
+        if model['estimator'].named_steps['normalization']:
+            unseen_data = pd.DataFrame(model['estimator'].named_steps['normalization'].transform(unseen_data), columns=features_name)
+
+        failure_prone = bool(tree_clf.predict(unseen_data)[0])
+
+        if failure_prone:
+
+            decision = []
+            decision_path = tree_clf.decision_path(unseen_data)
+            level_length = len(decision_path.indices)
+            i = 1
+            for node_id in decision_path.indices:
+                # Ignore last level because it is the last node
+                # without decision criteria or rule
+                if i < level_length:
+                    col_name = unseen_data.columns[tree_clf.tree_.feature[node_id]]
+                    threshold_value = round(tree_clf.tree_.threshold[node_id], 2)
+                    original_value = metrics.get(col_name, 0)
+
+                    # Inverse normalize threshold to make it more comprehensible to the final user
+                    normalized_value = unseen_data[col_name].values[0] if unseen_data[col_name].values[0] > 0 else 1
+                    threshold_value *= original_value/normalized_value
+
+                    decision.append((col_name, '<=' if original_value <= threshold_value else '>', threshold_value))
+
+                i += 1
+
+            response["decision"] = decision
+
+        response['failure_prone'] = failure_prone
+
         return response
 
-    return send_file(model, as_attachment=True)
+    elif language == 'tosca':
+        response["ERROR"] = f'Support for TOSCA coming soon.'
+        return response
+    else:
+        response["ERROR"] = f'Language {language} not supported.'
+        return response
 
 
 # A welcome message to test our server
